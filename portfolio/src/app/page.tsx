609350b70e0100f8187092ddfa5545dff411eb2b
shubham <shubhpatil8510@gmail.com>
2025-07-23 01:57:14 +0530

    Added project and footer component

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Project from "@/components/Project";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -9,7 +11,9 @@ export default function Home() {
       <Hero />
       <Experience />
       <Skills />
+      <Project />
       <Contact />
+      <Footer />
     </div>
   );
 }
