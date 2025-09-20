b7d5c03f3d4fe25aa0a69a323bd1b37991089688
shubham <shubhpatil8510@gmail.com>
2025-09-20 23:11:50 +0530

    Added a hover effect on skill btn

--- portfolio/src/components/Project.tsx
+++ portfolio/src/components/Project.tsx
@@ -235,7 +235,7 @@ const Project = () => {
                   {project.technologies.map((tech, i) => (
                     <div
                       key={i}
-                      className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80"
+                      className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-gray-200"
                     >
                       {tech}
                     </div>
